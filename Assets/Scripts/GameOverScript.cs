f480a5d9f98934ca560c3b90bf8dc10b4bc92411
Sean Liu <sean.y.liu@gmail.com>
2014-08-03 18:59:43 -0700

    fixed game over

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -21,27 +21,26 @@ public class GameOverScript : MonoBehaviour {
 	}
 	
 	void OnGUI() {
-		const int buttonWidth = 600;
-		const int buttonHeight = 200;
+		const int buttonWidth = 300;
+		const int buttonHeight = 100;
 		
 		// Set the skin to use
 		GUI.skin = skin;
-		
+
 		if (GUI.Button(
-			// Center in X, 1/2 of the height in Y
-			new Rect(
-			Screen.width / 2 - ((Screen.width / 2) / 2),
-			(1 * Screen.height / 2) - ((Screen.width / 2 * 49 / 190) / 2),
-			Screen.width / 2,
-			(Screen.width / 2 * 49 / 190)
-			),
-			"",
-			btnStyle
-			)) {
+				// Center in X, 2/3 of the height in Y
+				new Rect(
+					Screen.width / 2 - (buttonWidth / 2),
+					(Screen.height / 3) - (buttonHeight / 2),
+					buttonWidth,
+					buttonHeight
+				),
+				"Replay"
+				)) {
 			// Reload the level
-			Application.LoadLevel("Stage1");
+			Application.LoadLevel("MainScene");
 		}
-		
+
 		GUI.Label(new Rect(creditsPos.x, creditsPos.y, creditsSize.x, creditsSize.y), "Game by Rajeev Nayak & Sean Liu");
 	}
 }
